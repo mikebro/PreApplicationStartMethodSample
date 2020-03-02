8f7379c27bdc985391f3819e23599c1cea2727d9
Michael Broadbent <michael.broadbent@amilia.com>
2020-03-02 16:26:44 -0500

    First commit

---
+++ PreApplicationStartLibrary/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreApplicationStartLibrary
+{
+    public static class Startup
+    {
+        public static void Register()
+        {
+            Debugger.Launch();
+        }
+    }
+}
